c2092b4c121c4100fc9e58f3a5e71201f46d83e5
SketchBuch <sb.design.de@gmail.com>
2021-01-12 22:27:38 +0100

    feat(step-2): wip

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,23 @@
 const functions = require('firebase-functions');
 const app = require('express')();
-
 const {
   deleteTodo,
   editTodo,
   getAllTodos,
-  postOneTodo
+  postOneTodo,
 } = require('./apis/todos')
+
+const {
+  loginUser,
+  signUpUser,
+} = require('./apis/users')
 
 app.delete('/todo/:todoId', deleteTodo);
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.put('/todo/:todoId', editTodo);
+
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyC_EAxKebnfomiHWU0mSRNDBcvft9d-G2c",
+  authDomain: "reacttodo-58d05.firebaseapp.com",
+  databaseURL: "https://reacttodo-58d05-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "reacttodo-58d05",
+  storageBucket: "reacttodo-58d05.appspot.com",
+  messagingSenderId: "104625108081",
+  appId: "1:104625108081:web:3b82d01e9345afa60d57d0",
+  measurementId: "G-X8PYF441Q8",
+};
