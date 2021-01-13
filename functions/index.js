68a19073a2b1a77f5f77ff287af4f8975fd485ac
SketchBuch <sb.design.de@gmail.com>
2021-01-13 17:32:13 +0100

    chore: add user images

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,22 @@ const {
   getAllTodos,
   postOneTodo,
 } = require('./apis/todos')
-
+const auth = require('./utils/auth');
 const {
   loginUser,
   signUpUser,
+  uploadProfilePhoto,
 } = require('./apis/users')
 
+// Todos
 app.delete('/todo/:todoId', deleteTodo);
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.put('/todo/:todoId', editTodo);
 
+// Users
 app.post('/login', loginUser);
 app.post('/signup', signUpUser);
+app.post('/user/image', auth, uploadProfilePhoto);
 
 exports.api = functions.https.onRequest(app);
