1b5c871b622234a8558aa44bdb57227317570263
SketchBuch <sb.design.de@gmail.com>
2021-01-13 20:17:33 +0100

    feat: styling

--- view/src/components/App/App.js
+++ view/src/components/App/App.js
@@ -1,19 +1,32 @@
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomePage } from '../../pages/Home';
 import { LoginPage } from '../../pages/Login';
 import { SignupPage } from '../../pages/Signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#FF5722',
+      dark: '#d50000',
+      contrastText: '#fff'
+    }
+  }
+});
+
 export const App = () => {
   return (
-    <Router>
-      <div>
+    <MuiThemeProvider theme={theme}>
+      <Router>
         <Switch>
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/signup" component={SignupPage} />
           <Route exact path="/" component={HomePage} />
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </MuiThemeProvider>
   );
 }
